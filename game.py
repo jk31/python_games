75a2ba1fbf021df072d2940721c42d5e2864162e
JK <jarekkornowicz@googlemail.com>
2019-12-29 21:20:54 +0100

    wall checks, before sprite classes test

--- game.py
+++ game.py
@@ -4,15 +4,17 @@ from pygame.locals import *
 
 pg.init()
 fpsClock = pg.time.Clock()
-surface = pg.display.set_mode((800, 600))
+SIZE_X, SIZE_Y = 800, 600
+surface = pg.display.set_mode((SIZE_X, SIZE_Y))
 surfsize = surface.get_size()
 pg.display.set_caption("Game")
 
 
 class Obj():
-    def __init__(self, x, y, img, height, width):
+    def __init__(self, x, y, speed, img, height, width):
         self.x = x
         self.y = y
+        self.speed = speed
         self.height = height
         self.width = width
         self.img = pg.transform.scale(pg.image.load(img), (self.height, self.width))
@@ -27,8 +29,63 @@ class Obj():
         return f"{self.x}, {self.y}"
 
 
-bird = Obj(50, 50, "black.png", 60, 60)
-box = Obj(400, 200, "red.png", 80, 80)
+class Bird(Obj):
+    def __init__(self, x, y, speed, img, height, width):
+        super().__init__(x, y, speed, img, height, width)
+        self.status = "Is a bird."
+
+    def moving(self, keys):
+        if (keys[pg.K_w] and keys[pg.K_d]):
+            self.y += -self.speed[1]
+            self.x += self.speed[0]
+        elif (keys[pg.K_s] and keys[pg.K_d]):
+            self.y += self.speed[1]
+            self.x += self.speed[0]
+        elif (keys[pg.K_s] and keys[pg.K_a]):
+            self.y += self.speed[1]
+            self.x += -self.speed[0]
+        elif (keys[pg.K_w] and keys[pg.K_a]):
+            self.y += -self.speed[1]
+            self.x += -self.speed[0]
+        elif keys[pg.K_w]:
+            self.y += -self.speed[1]
+        elif keys[pg.K_s]:
+            self.y += self.speed[1]
+        elif keys[pg.K_d]:
+            self.x += self.speed[0]
+        elif keys[pg.K_a]:
+            self.x += -self.speed[0]
+
+        # wall check
+        if self.x+(self.width/2) > SIZE_X:
+            self.x = SIZE_X
+        if self.x < 0:
+            self.x = 0
+        if self.y > SIZE_Y:
+            self.y = SIZE_Y
+        if self.y < 0:
+            self.y = 0
+
+
+class Box(Obj):
+    def __init__(self, x, y, speed, img, height, width):
+        super().__init__(x, y, speed, img, height, width)
+        self.status = "Is a box."
+
+    def moving(self):
+        self.x += self.speed[0]
+        self.y += self.speed[1]
+
+        # wall check, speed = direction is reversed on the dimension
+        if (self.rect.right >= SIZE_X and self.speed[0] > 0) or (self.rect.left <= 0 and self.speed[0] < 0):
+            self.speed[0] = -self.speed[0]
+        if (self.rect.bottom >= SIZE_Y and self.speed[1] > 0) or (self.rect.top <= 0 and self.speed[1] < 0):
+            self.speed[1] = -self.speed[1]
+
+
+# create objects manually
+bird = Bird(50, 50, [10, 10], "black.png", 60, 60)
+box = Box(250, 200, [6, 6], "red.png", 80, 80)
 
 while True:
     surface.fill((255, 255, 255))
@@ -39,27 +96,16 @@ while True:
             sys.exit()
         #elif event.type == MOUSEMOTION:
             #print("ok")
-
     keys = pg.key.get_pressed()  #checking pressed keys
-    if keys[pg.K_UP] or keys[pg.K_w]:
-        bird.y += -3
-        print(bird)
-    if keys[pg.K_DOWN] or keys[pg.K_s]:
-        bird.y += 3
-        print(bird)
-    if (keys[pg.K_LEFT] or keys[pg.K_a]):
-        bird.x += -3
-        print(bird)
-    if (keys[pg.K_RIGHT] or keys[pg.K_d]):
-        bird.x += 3
-        print(bird)
 
-    box.x += 1
-    box.y += 1
+    bird.moving(keys)
 
+    # redraw objects
     box.draw()
     bird.draw()
 
+    box.moving()
+    # detect if boxes hit
     if bird.rect.colliderect(box.rect):
         print("HIT")
 
