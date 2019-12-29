6f968cd47808995b81fbf4eb1ad637847b7058f0
JK <jarekkornowicz@googlemail.com>
2019-12-29 22:55:33 +0100

    game over screen

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# flappy_bird
-A python implementation of the game Flappy Bird.
+# python games
+
+A few games
--- game.py
+++ game.py
@@ -1,80 +1,76 @@
 import pygame as pg
-import os, sys, random, math
+import os, sys, random, math, time
 from pygame.locals import *
 
 pg.init()
 fpsClock = pg.time.Clock()
 SIZE_X, SIZE_Y = 800, 600
 surface = pg.display.set_mode((SIZE_X, SIZE_Y))
+font = pg.font.Font(None, 36)
 surfsize = surface.get_size()
 pg.display.set_caption("Game")
 
 
-class Obj():
-    def __init__(self, x, y, speed, img, height, width):
-        self.x = x
-        self.y = y
+class Bird(pg.sprite.Sprite):
+    def __init__(self, speed):
+        pg.sprite.Sprite.__init__(self)
+
+        self.image = pg.Surface([10, 10])
+        self.image.fill((0, 0, 0))
+
         self.speed = speed
-        self.height = height
-        self.width = width
-        self.img = pg.transform.scale(pg.image.load(img), (self.height, self.width))
-        self.size = self.img.get_size()
-        self.rect = self.img.get_rect(center=(self.x, self.y))
-
-    def draw(self):
-        self.rect = self.img.get_rect(center=(self.x, self.y))
-        surface.blit(self.img, self.rect)
-
-    def __repr__(self):
-        return f"{self.x}, {self.y}"
-
-
-class Bird(Obj):
-    def __init__(self, x, y, speed, img, height, width):
-        super().__init__(x, y, speed, img, height, width)
         self.status = "Is a bird."
+
+        self.rect = self.image.get_rect()
 
     def moving(self, keys):
         if (keys[pg.K_w] and keys[pg.K_d]):
-            self.y += -self.speed[1]
-            self.x += self.speed[0]
+            self.rect.y += -self.speed[1]
+            self.rect.x += self.speed[0]
         elif (keys[pg.K_s] and keys[pg.K_d]):
-            self.y += self.speed[1]
-            self.x += self.speed[0]
+            self.rect.y += self.speed[1]
+            self.rect.x += self.speed[0]
         elif (keys[pg.K_s] and keys[pg.K_a]):
-            self.y += self.speed[1]
-            self.x += -self.speed[0]
+            self.rect.y += self.speed[1]
+            self.rect.x += -self.speed[0]
         elif (keys[pg.K_w] and keys[pg.K_a]):
-            self.y += -self.speed[1]
-            self.x += -self.speed[0]
+            self.rect.y += -self.speed[1]
+            self.rect.x += -self.speed[0]
         elif keys[pg.K_w]:
-            self.y += -self.speed[1]
+            self.rect.y += -self.speed[1]
         elif keys[pg.K_s]:
-            self.y += self.speed[1]
+            self.rect.y += self.speed[1]
         elif keys[pg.K_d]:
-            self.x += self.speed[0]
+            self.rect.x += self.speed[0]
         elif keys[pg.K_a]:
-            self.x += -self.speed[0]
+            self.rect.x += -self.speed[0]
 
         # wall check
-        if self.x+(self.width/2) > SIZE_X:
-            self.x = SIZE_X
-        if self.x < 0:
-            self.x = 0
-        if self.y > SIZE_Y:
-            self.y = SIZE_Y
-        if self.y < 0:
-            self.y = 0
+        if self.rect.right> SIZE_X:
+            self.rect.x = SIZE_X -self.image.get_width()
+        if self.rect.left < 0:
+            self.rect.x = 0
+        if self.rect.bottom > SIZE_Y:
+            self.rect.y = SIZE_Y - self.image.get_height()
+        if self.rect.top < 0:
+            self.rect.y = 0
 
 
-class Box(Obj):
-    def __init__(self, x, y, speed, img, height, width):
-        super().__init__(x, y, speed, img, height, width)
-        self.status = "Is a box."
+class Box(pg.sprite.Sprite):
+    def __init__(self, speed = [5, 5]):
+        pg.sprite.Sprite.__init__(self)
+
+        self.image = pg.Surface([20, 20])
+        self.image.fill((255, 0, 0))
+
+        self.speed = speed
+        self.status = "Is a bird."
+
+        self.rect = self.image.get_rect()
 
     def moving(self):
-        self.x += self.speed[0]
-        self.y += self.speed[1]
+        self.rect.x += self.speed[0]
+        self.rect.y += self.speed[1]
 
         # wall check, speed = direction is reversed on the dimension
         if (self.rect.right >= SIZE_X and self.speed[0] > 0) or (self.rect.left <= 0 and self.speed[0] < 0):
@@ -82,32 +78,77 @@ class Box(Obj):
         if (self.rect.bottom >= SIZE_Y and self.speed[1] > 0) or (self.rect.top <= 0 and self.speed[1] < 0):
             self.speed[1] = -self.speed[1]
 
+def main():
 
-# create objects manually
-bird = Bird(50, 50, [10, 10], "black.png", 60, 60)
-box = Box(250, 200, [6, 6], "red.png", 80, 80)
+    sprite_list = pg.sprite.Group()
+    box_list = pg.sprite.Group()
 
-while True:
-    surface.fill((255, 255, 255))
+    # create objects manually
+    bird = Bird([10, 10])
+    bird.rect.x = 500
+    bird.rect.y = 500
+    sprite_list.add(bird)
 
-    for event in pg.event.get():
-        if event.type == QUIT:
-            pg.quit()
-            sys.exit()
-        #elif event.type == MOUSEMOTION:
-            #print("ok")
-    keys = pg.key.get_pressed()  #checking pressed keys
+    for i in range(10):
+        box = Box()
+        box.rect.x = random.randrange(SIZE_X)
+        box.rect.y = random.randrange(50)
+        box.speed = [random.choice([-8, -7, -6, -5, -4, 4, 5, 6, 7, 8]),
+                     random.choice([-8, -7, -6, -5, -4, 4, 5, 6, 7, 8])]
+        box_list.add(box)
+        sprite_list.add(box)
 
-    bird.moving(keys)
+    START = time.time()
+    while True:
+        surface.fill((255, 255, 255))
 
-    # redraw objects
-    box.draw()
-    bird.draw()
+        for event in pg.event.get():
+            if event.type == QUIT:
+                pg.quit()
+                sys.exit()
 
-    box.moving()
-    # detect if boxes hit
-    if bird.rect.colliderect(box.rect):
-        print("HIT")
 
-    pg.display.update()
-    fpsClock.tick(30)
+        keys = pg.key.get_pressed()  #checking pressed keys
+
+        bird.moving(keys)
+
+        sprite_list.draw(surface)
+
+        for box in box_list:
+            box.moving()
+
+        # detect if boxes hit
+        if pg.sprite.spritecollideany(bird, box_list):
+            print("HIT", time.time())
+            END = time.time()
+
+            # GAME OVER STATE
+            while True:
+                for event in pg.event.get():
+                    if event.type == QUIT:
+                        pg.quit()
+                        sys.exit()
+
+                keys = pg.key.get_pressed()
+
+                if keys[K_RETURN]:
+                    print("restart")
+                    main()
+
+                surface.fill((0, 0 ,0))
+                msg = f"Game Over - Survived {round(END - START, 2)} seconds"
+                text = font.render(msg, True, (255, 255, 255))
+                text_rect = text.get_rect()
+                text_x = surface.get_width() / 2 - text_rect.width / 2
+                text_y = surface.get_height() / 2 - text_rect.height / 2
+
+                surface.blit(text, [text_x, text_y])
+
+                pg.display.update()
+                fpsClock.tick(30)
+
+        pg.display.update()
+        fpsClock.tick(30)
+
+if __name__ == '__main__':
+    main()
